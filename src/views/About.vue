e466df6c287dab9e20eab553d07501be38b0a349
Denys Fesenko <dfesuk@gmail.com>
2021-07-11 13:11:20 +0300

    :sparkles: add button 'plus&delete'

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus amet animi velit nobis dicta odio atque assumenda aspernatur illo laudantium beatae magni placeat fugit, ipsam veniam blanditiis, labore explicabo. Enim distinctio veniam, iure temporibus odio at quas porro doloribus? Labore architecto laborum, veritatis perspiciatis dolorem enim exercitationem corporis autem porro.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus amet animi velit nobis dicta odio atque assumenda aspernatur illo laudantium beatae magni placeat fugit, ipsam veniam blanditiis, labore explicabo. Enim distinctio veniam, iure temporibus odio at quas porro doloribus? Labore architecto laborum, veritatis perspiciatis dolorem enim exercitationem corporis autem porro.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus amet animi velit nobis dicta odio atque assumenda aspernatur illo laudantium beatae magni placeat fugit, ipsam veniam blanditiis, labore explicabo. Enim distinctio veniam, iure temporibus odio at quas porro doloribus? Labore architecto laborum, veritatis perspiciatis dolorem enim exercitationem corporis autem porro.</p>
   </div>
 </template>
